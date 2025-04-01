21295efda3b7427e3cb6d418dd0f8a8a02b592f7
Isuzu <ballage94@gmail.com>
2025-04-02 00:23:32 +0300

    fix(sidebar): selection logic

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -5,6 +5,7 @@ import { items } from '../../constants/sidebar';
 import SidebarItem from './SidebarItem';
 const Sidebar = () => {
     const [activeItem, setActiveItem] = useState<string>('');
+    const [activeChild, setActiveChild] = useState<string>('');
 
     return (
         <Accordion allowToggle p='10px 16px 10px 10px' bgColor='white'>
@@ -14,6 +15,8 @@ const Sidebar = () => {
                     item={item}
                     activeItem={activeItem}
                     setActiveItem={setActiveItem}
+                    activeChild={activeChild}
+                    setActiveChild={setActiveChild}
                 />
             ))}
         </Accordion>
--- src/app/components/Sidebar/SidebarItem.tsx
+++ src/app/components/Sidebar/SidebarItem.tsx
@@ -6,7 +6,6 @@ import {
     Box,
     Image,
 } from '@chakra-ui/react';
-import { useState } from 'react';
 
 import SidebarItemChild from './SidebarItemChild';
 
@@ -14,39 +13,39 @@ const SidebarItem = ({
     item,
     activeItem,
     setActiveItem,
+    activeChild,
+    setActiveChild,
 }: {
     item: { icon: string; label: string; children: string[] };
     activeItem: string;
     setActiveItem: (item: string) => void;
-}) => {
-    const [activeChild, setActiveChild] = useState<string>('');
-
-    return (
-        <AccordionItem key={item.label} onClick={() => setActiveItem(item.label)}>
-            <AccordionButton
-                border='0px'
-                p='12px 8px'
-                bgColor={activeItem === item.label ? '#EAFFC7' : 'white'}
-                cursor='pointer'
-            >
-                <Image src={item.icon} />
-                <Box as='span' flex='1' textAlign='left' margin='auto 12px'>
-                    {item.label}
-                </Box>
-                <AccordionIcon size='16px' />
-            </AccordionButton>
-            <AccordionPanel>
-                {item.children.map((child: string) => (
-                    <SidebarItemChild
-                        key={child}
-                        child={child}
-                        activeChild={activeChild}
-                        setActiveChild={setActiveChild}
-                    />
-                ))}
-            </AccordionPanel>
-        </AccordionItem>
-    );
-};
+    activeChild: string;
+    setActiveChild: (child: string) => void;
+}) => (
+    <AccordionItem key={item.label} onClick={() => setActiveItem(item.label)}>
+        <AccordionButton
+            border='0px'
+            p='12px 8px'
+            bgColor={activeItem === item.label ? '#EAFFC7' : 'white'}
+            cursor='pointer'
+        >
+            <Image src={item.icon} />
+            <Box as='span' flex='1' textAlign='left' margin='auto 12px'>
+                {item.label}
+            </Box>
+            <AccordionIcon size='16px' />
+        </AccordionButton>
+        <AccordionPanel>
+            {item.children.map((child: string) => (
+                <SidebarItemChild
+                    key={child}
+                    child={child}
+                    activeChild={activeChild}
+                    setActiveChild={setActiveChild}
+                />
+            ))}
+        </AccordionPanel>
+    </AccordionItem>
+);
 
 export default SidebarItem;
