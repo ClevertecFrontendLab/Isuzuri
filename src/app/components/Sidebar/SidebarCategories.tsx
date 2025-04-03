e12f0fcb4af2cde0ef01240e201960278d013d81
Isuzuri <ballage94@gmail.com>
2025-04-03 15:29:02 +0300

    style(sidebar): add box-shadow if open dropdown

--- src/app/components/Sidebar/SidebarCategories.tsx
+++ src/app/components/Sidebar/SidebarCategories.tsx
@@ -9,7 +9,13 @@ const SidebarCategories = () => {
     const [activeChild, setActiveChild] = useState<string>('');
 
     return (
-        <Accordion allowToggle p='10px 16px 10px 10px' overflowY='scroll'>
+        <Accordion
+            allowToggle
+            p='10px 16px 10px 10px'
+            overflowY='scroll'
+            boxShadow={activeItem && '0px 2px 4px -1px #0000000F, 0px 4px 6px -1px #0000001A'}
+            borderBottomRadius={activeItem && '12px'}
+        >
             {items.map((item: { icon: string; label: string; children: string[] }) => (
                 <SidebarItem
                     key={item.label}
